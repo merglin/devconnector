b6ef774061c9b4de7a3acd9e8b49ab07124c1bdd
Mike Gemmell <merglin@gmail.com>
2018-09-02 11:10:55 -0300

    Hook up profile state

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,19 +5,48 @@ import { getCurrentProfile } from "../../actions/profileActions";
 
 class Dashboard extends Component {
   componentDidMount() {
-    console.log("getting current profile");
     this.props.getCurrentProfile();
   }
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+    if (profile === null || loading) {
+      dashboardContent = <h4>Loading...</h4>;
+    } else {
+      dashboardContent = <h1>Hello</h1>;
+    }
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col=md-12">
+              <h1 clasName="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => {
+  console.log("mapping state to prop");
+  return {
+    profile: state.profile,
+    auth: state.auth
+  };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { getCurrentProfile }
 )(Dashboard);
