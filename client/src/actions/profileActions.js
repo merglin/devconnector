f942528b9932547a7547493d21d59486573703a3
Mike Gemmell <merglin@gmail.com>
2018-09-03 19:23:34 -0300

    Create ProfileActions component to display on dashboard

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -2,7 +2,8 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   GET_ERRORS,
-  CLEAR_CURRENT_PROFILE
+  CLEAR_CURRENT_PROFILE,
+  SET_CURRENT_USER
 } from "./types";
 
 import axios from "axios";
@@ -35,6 +36,25 @@ export const createProfile = (profileData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+export const deleteAccount = () => dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone")) {
+    axios
+      .delete("/api/profile")
+      .then(res => {
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        });
+      })
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 };
 
 export const setProfileLoading = () => {
